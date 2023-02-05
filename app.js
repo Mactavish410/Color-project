534ea2b38408e38b5b0043aea8dd85799c4802f5
Mactavish410 <Mactavish410@gmail.com>
2023-02-05 22:45:22 +0500

    setRandom

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const cols = document.querySelectorAll(".col")
 
-function se
+function setRandomColors() {
+    cols.forEach((col) => {
+        const text = col.querySelector('h2')
+        let color = genereteRandomColor()
+        text.textContent = color
+        col.style.background = color
+    })
+}
+
+function genereteRandomColor() {
+    const hexCode = '0123456789ABCDEF'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        color += hexCode[Math.floor(Math.random() * hexCode.length)]
+    }
+    return '#' + color
+}
+    
+
+
+setRandomColors()
+genereteRandomColor()
